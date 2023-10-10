0a21ebc3a86c67ba8c28a67d370ef7389105b7cf
RathikaJayakumar <144705491+RathikaJayakumar@users.noreply.github.com>
2023-10-10 20:22:06 +0530

    Add files via upload

---
+++ ArrayListExample.java
@@ -0,0 +1,20 @@
+package std;
+import java.util.*;
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				ArrayList <Integer> a1=new ArrayList<Integer>();
+				a1.add(100);
+				a1.add(200);
+				a1.add(300);
+				a1.add(400);
+				a1.add(500);
+				for(Integer i:a1)
+				{
+					System.out.println(i);
+				}
+
+	}
+
+}
